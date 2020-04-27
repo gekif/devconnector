d7d32dc4ade649d6f7d5550e3b6d80c4abd7402e
Gekif <dzulfikar.maulana@gmail.com>
2020-04-27 17:06:08 +0700

    03 User API Routes  JWT Authentication: 011 Email  Password Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,7 +14,7 @@ const User = require('../../models/User');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: "Users Works" }));
 
-// @route   GET api/users/register
+// @route   POST api/users/register
 // @desc    Register
 // @access  Public
 router.post('/register', (req, res) => {
@@ -49,7 +49,35 @@ router.post('/register', (req, res) => {
                     });
                 });
             }
-        })
+        });
+});
+
+// @route   POST api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find User By Email
+    User.findOne({ email: email })
+        .then(user => {
+            // Check For User
+            if (!user) {
+                return res.status(404).json({ email: 'User not found'});
+            }
+
+            // Check Password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: 'Success' });
+
+                    } else {
+                        return res.status(400).json({ password: 'Password incorrect' })
+                    }
+                });
+        });
 });
 
 
