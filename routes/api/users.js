d259c54a8f3d3b39711dbe46ae1ff6947087d1ab
Gekif <dzulfikar.maulana@gmail.com>
2020-04-27 17:17:39 +0700

    03 User API Routes  JWT Authentication: 012 Creating The JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -71,7 +73,20 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({ msg: 'Success' });
+                        // User Matched
+                        const payload = { id: user.id , name: user.name, avatar: user.avatar}; // Create JWT Payload
+
+                        // Sign Token
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            });
 
                     } else {
                         return res.status(400).json({ password: 'Password incorrect' })
