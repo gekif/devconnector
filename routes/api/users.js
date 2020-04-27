ee1d660b2fe298bfd7f24b94471a46214ca96542
Gekif <dzulfikar.maulana@gmail.com>
2020-04-27 20:44:37 +0700

    03 User API Routes  JWT Authentication: 015 Validation Handlers - 2

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,6 +10,7 @@ const router = express.Router();
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 
 // Load User Mode
@@ -35,7 +36,9 @@ router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                return res.status(400).json({ email: 'Email already exists' });
+                errors.email = 'Email already exists';
+
+                return res.status(400).json(errors);
 
             } else {
                 const avatar = gravatar.url(req.body.email, {
@@ -70,6 +73,13 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -78,7 +88,9 @@ router.post('/login', (req, res) => {
         .then(user => {
             // Check For User
             if (!user) {
-                return res.status(404).json({ email: 'User not found'});
+                errors.email = 'User not found';
+
+                return res.status(404).json(errors);
             }
 
             // Check Password
@@ -101,7 +113,9 @@ router.post('/login', (req, res) => {
                             });
 
                     } else {
-                        return res.status(400).json({ password: 'Password incorrect' })
+                        errors.password = 'Password incorrect';
+
+                        return res.status(400).json(errors);
                     }
                 });
         });
