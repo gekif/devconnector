7e72e2045c9e7b8cc324c0d1dae1465131b70ff1
Gekif <dzulfikar.maulana@gmail.com>
2020-05-03 17:34:01 +0700

    04 Profile API Routes: 018 Current User Profile Route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 const router = express.Router();
 
 
-// @route   GET api/profiles/test
-// @desc    Test post route
+// Load Profile Model
+const Profile = require('../../models/Profile');
+
+// Load User Profile
+const User = require('../../models/User');
+
+
+// @route   GET api/profile/test
+// @desc    Tests profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: "Profiles Works" }));
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile';
+
+                return res.status(404).json(errors)
+            }
+
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
 
 module.exports = router;
