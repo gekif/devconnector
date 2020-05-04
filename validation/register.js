fa35082f2221a2f889163e30502f19b4895ad655
Gekif <dzulfikar.maulana@gmail.com>
2020-05-04 18:46:35 +0700

    04 Profile API Routes: 022 Add Experience  Education Routes

--- validation/register.js
+++ validation/register.js
@@ -1,4 +1,4 @@
-const validator = require('validator');
+const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
 
@@ -10,35 +10,35 @@ module.exports = function validateRegisterInput(data) {
     data.password = !isEmpty(data.password) ? data.password : '';
     data.confirmPassword = !isEmpty(data.confirmPassword) ? data.confirmPassword : '';
 
-    if (!validator.isLength(data.name, { min: 2, max: 30 })) {
+    if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
         errors.name = 'Name must be between 2 and 30 characters';
     }
 
-    if (validator.isEmpty(data.name)) {
+    if (Validator.isEmpty(data.name)) {
         errors.name = 'Name field is required';
     }
 
-    if (validator.isEmpty(data.email)) {
+    if (Validator.isEmpty(data.email)) {
         errors.email = 'Email field is required';
     }
 
-    if (!validator.isEmail(data.email)) {
+    if (!Validator.isEmail(data.email)) {
         errors.email = 'Email is invalid';
     }
 
-    if (validator.isEmpty(data.password)) {
+    if (Validator.isEmpty(data.password)) {
         errors.password = 'Password field is required';
     }
 
-    if (!validator.isLength(data.password, { min: 6, max: 30 })) {
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
         errors.password = 'Password must be at least 6 characters';
     }
 
-    if (validator.isEmpty(data.confirmPassword)) {
+    if (Validator.isEmpty(data.confirmPassword)) {
         errors.confirmPassword = 'Confirm password field is required';
     }
 
-    if (!validator.equals(data.password, data.confirmPassword)) {
+    if (!Validator.equals(data.password, data.confirmPassword)) {
         errors.confirmPassword = 'Password must match';
     }
 
