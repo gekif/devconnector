9fc616a573fec274f613d7a18d7e8fe1ad42cd6f
Gekif <dzulfikar.maulana@gmail.com>
2020-05-10 06:45:23 +0700

    05 Post API Routes: 028 Add  Remove Comment Routes-en

--- models/Post.js
+++ models/Post.js
@@ -48,6 +48,10 @@ const PostSchema = new Schema({
             avatar: {
                 type: String
             },
+            date: {
+                type: Date,
+                default: Date.now()
+            }
         }
     ],
 
