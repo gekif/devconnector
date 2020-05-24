77b6822ce8e610d50f6d81b5811c5f5252983fde
Gekif <dzulfikar.maulana@gmail.com>
2020-05-24 22:14:05 +0700

    08 Redux  Authentication: 043 Login Form Functionality 044 Logout  Conditional Navbar Links

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -12,27 +16,49 @@ import Login from './components/auth/Login';
 import './App.css';
 
 
-class App extends Component {
-    render() {
-        return (
-            <Provider store={ store }>
-                <Router>
-                    <div className="App">
-                        <Navbar />
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
 
-                        <Route exact path="/" component={ Landing } />
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
 
-                        <div className="container">
-                            <Route exact path="/register" component={ Register } />
-                            <Route exact path="/login" component={ Login } />
-                        </div>
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
 
-                        <Footer />
-                    </div>
-                </Router>
-            </Provider>
-        );
-    }
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // TODO: Clear current Profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
 
 export default App;
