2f4c33a7db38b6e3f5cd22f9f714259cdd5f0484
Gekif <dzulfikar.maulana@gmail.com>
2020-05-26 09:49:39 +0700

    09 Dashboard  Profile State - Part 1 : 051 Create Profile Form 052 Create Profile Functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import {
+    GET_PROFILE,
+    PROFILE_LOADING,
+    CLEAR_CURRENT_PROFILE,
+    GET_ERRORS
+} from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
     dispatch(setProfileLoading());
-
     axios
         .get('/api/profile')
         .then(res =>
@@ -19,7 +23,20 @@ export const getCurrentProfile = () => dispatch => {
                 type: GET_PROFILE,
                 payload: {}
             })
-        )
+        );
+};
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+        .post('/api/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
 };
 
 // Profile loading
